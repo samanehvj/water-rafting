e77cd5dde67e6443ff4a1c758f168768637ae1de
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2023-06-28 13:33:27 -0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<script src="js/slide.js"></script>
+<script src="js/form_validate.js"></script>
+<script src="https://kit.fontawesome.com/a424e00da0.js" crossorigin="anonymous"></script>
+
+
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small py-4">
+
+
+  <!-- Copyright -->
+  <div class="footer-copyright bg-warning text-center mr-3 py-4 "> All rights &copy; reserved by Samaneh Vajdi 2020
+    <!-- Facebook -->
+    <a class="fb-ic">
+      <i class="fab fa-facebook-f fa-lg white-text ml-3 ml-1 mr-md-5 mr-2 fa-2x"> </i>
+    </a>
+
+    <!--Instagram-->
+    <a class="ins-ic">
+      <i class="fab fa-instagram fa-lg white-text mr-md-3  mr-5 fa-2x"> </i>
+    </a>
+
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+
+
+
+
+</body>
+
+</html>
