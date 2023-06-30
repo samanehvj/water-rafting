de92e4625399d83bf11befe72c6a8d1b696a3583
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2023-06-30 10:30:07 -0700

    Create slide.js

---
+++ js/slide.js
@@ -0,0 +1,46 @@
+function alertFunction() {
+  alert("The form submited successfully");
+}
+
+var slideIndex = 1;
+//showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+
+  var slides = document.getElementsByClassName("mySlides");
+  // var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  index = slideIndex - 1;
+  slides[index].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
+}
+
+function startSlide() {
+  //var i=2;
+  // call the slideshow.nextSlide method every 2 seconds
+  showSlides.interval = setInterval(function () {
+    showSlides(++slideIndex);
+  }, 3000);
+}
+// startSlide();
+// showSlides();
